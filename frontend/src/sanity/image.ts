0a272a862c45678b78804bda7f9ee14e7dd5612c
Nonso Anetoh <anet0001@algonquinlive.com>
2026-06-20 11:39:22 -0700

    chore: remove legacy frontend monorepo package

--- frontend/src/sanity/image.ts
+++
@@ -1,9 +0,0 @@
-import createImageUrlBuilder from '@sanity/image-url'
-import type { SanityImageSource } from '@sanity/image-url/lib/types/types'
-import { client } from './client'
-
-const imageBuilder = createImageUrlBuilder(client)
-
-export function urlFor(source: SanityImageSource) {
-  return imageBuilder.image(source)
-}
